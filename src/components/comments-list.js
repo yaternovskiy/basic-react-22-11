7eb83063110c38e73639252b4a53dd8ca4379118
boo <ya.ternovskiy@gmail.com>
2018-11-28 23:47:18 +0300

    HT1.2 collapse comments

--- src/components/comments-list.js
+++ src/components/comments-list.js
@@ -2,17 +2,37 @@ import React, { Component } from 'react'
 import Comment from './comment'
 
 class CommentsList extends Component {
+  state = {
+    isExpanded: false
+  }
   render() {
-    return <ul>{this.commentItems()}</ul>
+    return <div>{this.commentItems()}</div>
+  }
+
+  toggleExpand = () => {
+    this.setState({ isExpanded: !this.state.isExpanded })
   }
 
   commentItems() {
     const { comments } = this.props
-    return comments.map((comment) => (
-      <li key={comment.id}>
-        <Comment comment={comment} />
-      </li>
-    ))
+    return (
+      comments && (
+        <div style={{ marginTop: '20px' }}>
+          <button onClick={this.toggleExpand}>
+            {this.state.isExpanded ? 'Hide comments' : 'Show comments (' + comments.length + ')'}
+          </button>
+          {this.state.isExpanded && (
+            <ul>
+              {comments.map((comment) => (
+                <li key={comment.id}>
+                  <Comment comment={comment} />
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      )
+    )
   }
 }
 
